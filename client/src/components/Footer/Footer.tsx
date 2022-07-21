40d6abd5f99fc68580e577230a8aa5bc1b2adcf9
Asmita Gaire <asmitagaire00@gmail.com>
2022-07-21 16:53:23 +1000

    update Footer component

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div className="bg-black w-full text-white text-sm  md:pl-12 pl-4 flex flex-col md:flex-row justify-between flex-wrap p-[1rem] mb-0  border rounded-t-[42px]">
+      <h1 className="font-mysteryQuest text-sm md:text-2xl py-2 ">
+        CatWiki 🐈
+      </h1>
+      <p className="font-montserrat text-xs md:text-lg md:pr-4 pb-1">
+        © Created by <b>Asmita</b> 2022
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
