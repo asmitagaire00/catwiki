bcb5e66003653a7d2c7509fb7308c90e02bff287
Asmita Gaire <asmitagaire00@gmail.com>
2022-07-21 21:42:44 +1000

    update Footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-black w-full text-white text-sm  md:pl-12 pl-4 flex flex-col md:flex-row justify-between flex-wrap p-[1rem] mb-0  border rounded-t-[42px]">
+    <div className="bg-black w-full text-white text-sm  md:pl-12 flex flex-col md:flex-row justify-between flex-wrap p-[1rem] mb-0  border rounded-t-[42px]">
       <h1 className="font-mysteryQuest text-sm md:text-2xl py-2 ">
         CatWiki 🐈
       </h1>
