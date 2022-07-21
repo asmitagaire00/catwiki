e27661d12258cd58129c1f3924e8bafb59ad6442
Asmita Gaire <asmitagaire00@gmail.com>
2022-07-21 19:35:40 +1000

    add HeroTopCard

---
+++ client/src/components/HeroTopCard/HeroTopCard.tsx
@@ -0,0 +1,24 @@
+import Search from "../Search/Search";
+
+const HeroTopCard = () => {
+  return (
+    <div className="relative">
+      <img
+        src="assets/HeroImagelg.png"
+        alt="hero"
+        className="w-full object-contain border rounded-t-[42px]"
+      />
+      <div className="text-white absolute top-6 left-5  md:top-[3rem] md:left-[3rem] lg:top-[5rem] lg:left-[5rem] ">
+        <p className="font-mysteryQuest text-sm md:text-[30px] lg:text-[40px] mb-1.5 md:mb-4 lg:mb-6">
+          Catwiki<span className="hidden md:inline-block">🐈</span>
+        </p>
+        <p className="text-[10px] md:text-xl lg:text-2xl mb-2.5 md:mb-4 lg:mb-6">
+          Get to know more about your cat breeds
+        </p>
+        <Search />
+      </div>
+    </div>
+  );
+};
+
+export default HeroTopCard;
