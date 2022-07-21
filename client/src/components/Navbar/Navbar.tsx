9f65190bcbc1d320602556dce53254df20412a85
Asmita Gaire <asmitagaire00@gmail.com>
2022-07-21 17:28:00 +1000

    add Navbar

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="">
+      <h1 className="font-mysteryQuest font-normal text-2xl text-titleColor">
+        CatWiki 🐈
+      </h1>
+    </nav>
+  );
+};
+
+export default Navbar;
